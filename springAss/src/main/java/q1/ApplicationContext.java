f416003f3a08db3746cf960c86942caa4e7f341c
SaiCharan1103 <92868531+SaiCharan1103@users.noreply.github.com>
2021-11-10 07:37:50 +0530

    Add files via upload

---
+++ springAss/src/main/java/q1/ApplicationContext.java
@@ -0,0 +1,10 @@
+package q1;
+
+public class ApplicationContext {
+
+	public Customer getBean(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ springAss/src/main/java/q1/test.java
@@ -0,0 +1,22 @@
+package q1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class test {
+	@SuppressWarnings("unused")
+	private static ApplicationContext context;
+	
+	public static void main(String[] args) {
+		 
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("q1bean.xml");
+		
+		//using setter injection
+		//Customer customer = (Customer)context.getBean("customer");
+		//customer.displayCustomer();
+		
+		Customer customer2 = (Customer) context.getBean("customerConstructor");
+		customer2.displayCustomer();		
+	}
+	}
